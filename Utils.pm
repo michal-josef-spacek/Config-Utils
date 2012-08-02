2c18d8b348b6e44fe4bf55e3396a671994267431
Michal Špaček <tupinek@gmail.com>
2012-08-02 16:12:13 +0200

    Implement hash_array().

--- Utils.pm
+++ Utils.pm
@@ -10,7 +10,7 @@ use Error::Pure qw(err);
 use Readonly;
 
 # Constants.
-Readonly::Array our @EXPORT_OK => qw(conflict hash);
+Readonly::Array our @EXPORT_OK => qw(conflict hash hash_array);
 
 # Version.
 our $VERSION = 0.02;
@@ -54,6 +54,41 @@ sub hash {
 	return;
 }
 
+# Create record to hash.
+sub hash_array {
+	my ($self, $key_ar, $val) = @_;
+	my @tmp = @{$key_ar};
+	my $tmp_hr = $self->{'config'};
+	foreach my $i (0 .. $#tmp) {
+		if ($i != $#tmp) {
+			if (! exists $tmp_hr->{$tmp[$i]}) {
+				$tmp_hr->{$tmp[$i]} = {};
+			} elsif (ref $tmp_hr->{$tmp[$i]} ne 'HASH') {
+				conflict($self, $tmp_hr, $tmp[$i]);
+				$tmp_hr->{$tmp[$i]} = {};
+			}
+			$tmp_hr = $tmp_hr->{$tmp[$i]};
+			push @{$self->{'stack'}}, $tmp[$i];
+		} else {
+			if (defined $self->{'callback'}) {
+				$tmp_hr->{$tmp[$i]}
+					= $self->{'callback'}->($val);
+			} else {
+				if (ref $tmp_hr->{$tmp[$i]} eq 'ARRAY') {
+					push @{$tmp_hr->{$tmp[$i]}}, $val;
+				} elsif ($tmp_hr->{$tmp[$i]}) {
+					my $foo = $tmp_hr->{$tmp[$i]};
+					$tmp_hr->{$tmp[$i]} = [$foo, $val];
+				} else {
+					$tmp_hr->{$tmp[$i]} = $val;
+				}
+			}
+			$self->{'stack'} = [];
+		}
+	}
+	return;
+}
+
 1;
 
 __END__
@@ -68,9 +103,10 @@ Config::Utils - Common config utilities.
 
 =head1 SYNOPSIS
 
- use Config::Utils qw(conflict hash);
+ use Config::Utils qw(conflict hash hash_array);
  conflict($self, {'key' => 1}, 'key');
  hash($self, ['one', 'two'], $val);
+ hash_array($self, ['one', 'two'], $val);
 
 =head1 SUBOUTINES
 
@@ -86,6 +122,12 @@ Config::Utils - Common config utilities.
  Create record to hash.
  Returns undef or fatal error.
 
+=item B<hash_array($self, $key_ar, $val)>
+
+ Create record to hash.
+ If exists more value record for one key, then create array of values.
+ Returns undef or fatal error.
+
 =back
 
 =head1 ERRORS
@@ -94,6 +136,9 @@ Config::Utils - Common config utilities.
          Conflict in '%s'.
 
  hash():
+         Conflict in '%s'.
+
+ hash_array():
          Conflict in '%s'.
 
 =head1 EXAMPLE1
@@ -151,6 +196,42 @@ Config::Utils - Common config utilities.
  #    'set_conflicts' => 1
  #    'stack' => ARRAY(0x955cc38)
  #         empty array 
+
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Config::Utils qw(hash_array);
+ use Dumpvalue;
+
+ # Object.
+ my $self = {
+         'config' => {},
+         'set_conflicts' => 1,
+         'stack' => [],
+ };
+
+ # Add records.
+ hash_array($self, ['foo', 'baz'], 'bar');
+ hash_array($self, ['foo', 'baz'], 'bar');
+
+ # Dump.
+ my $dump = Dumpvalue->new;
+ $dump->dumpValues($self);
+
+ # Output:
+ # 0  HASH(0x8edf890)
+ #    'config' => HASH(0x8edf850)
+ #       'foo' => HASH(0x8edf840)
+ #          'baz' => ARRAY(0x8edf6d0)
+ #             0  'bar'
+ #             1  'bar'
+ #    'set_conflicts' => 1
+ #    'stack' => ARRAY(0x8edf6e0)
+ #         empty array
 
 =head1 DEPENDENCIES
 
