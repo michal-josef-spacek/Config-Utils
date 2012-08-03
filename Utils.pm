4e3a893b5bd3477dec47da3aa0e6424b8645b5f4
Michal Špaček <tupinek@gmail.com>
2012-08-03 10:44:24 +0200

    Prepare for 0.03 release.

--- Utils.pm
+++ Utils.pm
@@ -13,7 +13,7 @@ use Readonly;
 Readonly::Array our @EXPORT_OK => qw(conflict hash hash_array);
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Check conflits.
 sub conflict {
@@ -259,6 +259,6 @@ BSD license.
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
--- t/Config-Utils/01-version.t
+++ t/Config-Utils/01-version.t
@@ -7,4 +7,4 @@ use Config::Utils;
 use Test::More 'tests' => 1;
 
 # Test.
-is($Config::Utils::VERSION, 0.02, 'Version.');
+is($Config::Utils::VERSION, 0.03, 'Version.');
